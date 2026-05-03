13aafacaf2c866b9d2faaf1111723ca147c9744f
jinyixuan1116 <2602145743@qq.com>
2026-05-03 14:45:29 +0800

    Add files via upload

---
+++ niuke-solutions/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, k, m;
+    cin >> n >> k >> m;
+    vector<int>num;
+    for (int i = 0; i < n; i++) {
+        num.push_back(i);
+    }
+    int current = k;
+    int a = num.size();
+    while (num.size() > 1) {
+        current = (current + m - 1) % a;
+        num.erase(num.begin() + current);
+        a--;
+    }
+    cout << num[0] << endl;
+
+    return 0;
+}
